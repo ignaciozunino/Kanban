2d75b655aa9baf8fa7229b231c902facaa51a768
gapoj <guilleapoj@gmail.com>
2015-04-20 17:05:50 -0300

    minor bug solved

--- Kanban/Controllers/ProjectPageViewController.m
+++ Kanban/Controllers/ProjectPageViewController.m
@@ -72,7 +72,7 @@
         newTask.taskDescription = [item objectForKey:@"description"];
         newTask.state = [item objectForKey:@"state"];
         
-        KBNProject *project = [[KBNProject alloc]initWithEntity:[NSEntityDescription entityForName:@"Project" inManagedObjectContext:self.managedObjectContext] insertIntoManagedObjectContext:self.managedObjectContext];
+        KBNProject *project = [[KBNProject alloc]initWithEntity:[NSEntityDescription entityForName:ENTITY_PROJECT inManagedObjectContext:self.managedObjectContext] insertIntoManagedObjectContext:self.managedObjectContext];
         
         project.name = [item objectForKey:@"project"];
         
