f06451cc7cf85a79fca00d2e6ec187d688353daf
Carlos Guerra <carlos.guerra@globant.com>
2015-07-30 15:09:02 -0500

    Cambios solicitados para lo de prioritytask

---
+++ Kanban/Controllers/Task/KBNTaskPriorityPickerView.h
@@ -0,0 +1,13 @@
+//
+//  KBNTaskPriorityPickerView.h
+//  Kanban
+//
+//  Created by guerrier on 30/07/15.
+//  Copyright (c) 2015 Globant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KBNTaskPriorityPickerView : UIPickerView
+
+@end
---
+++ Kanban/Controllers/Task/KBNTaskPriorityPickerView.m
@@ -0,0 +1,21 @@
+//
+//  KBNTaskPriorityPickerView.m
+//  Kanban
+//
+//  Created by guerrier on 30/07/15.
+//  Copyright (c) 2015 Globant. All rights reserved.
+//
+
+#import "KBNTaskPriorityPickerView.h"
+
+@implementation KBNTaskPriorityPickerView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
