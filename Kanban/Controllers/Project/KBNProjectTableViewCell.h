32a549ce7cb9cadf6a92693f8fbbed7a7f93a3dd
Carlos Guerra <carlos.guerra@globant.com>
2015-09-07 08:40:52 -0500

    Agregar priority de las task

---
+++ Kanban/Controllers/Project/KBNProjectTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  KBNProjectTableViewCell.h
+//  Kanban
+//
+//  Created by guerrier on 07/09/15.
+//  Copyright (c) 2015 Globant. All rights reserved.
+//
+
+#ifndef Kanban_KBNProjectTableViewCell_h
+#define Kanban_KBNProjectTableViewCell_h
+
+
+#endif
